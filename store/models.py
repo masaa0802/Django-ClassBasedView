0083b7a9f74fd2ad572a3f283a5f9320e86f5d4f
Your Name <masaakigoto0802@gmail.com>
2022-01-24 17:23:20 +0900

    ~231

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse_lazy
-
+from django.dispatch import receiver
+import os
 
 class BaseModel(models.Model):
   create_at = models.DateTimeField()
@@ -20,3 +21,21 @@ class Books(BaseModel):
 
   def get_absolute_url(self):
     return reverse_lazy('store:detail_book', kwargs={'pk':self.pk})
+
+class PicturesManager(models.Manager):
+  def filter_by_book(self, book):
+    return self.filter(book=book).all()
+
+class Pictures(BaseModel):
+
+  picture = models.FileField(upload_to='picture/')
+  book = models.ForeignKey(
+    'books', on_delete=models.CASCADE
+  )
+  objects = PicturesManager()
+
+@receiver(models.signals.post_delete, sender=Pictures)
+def delete_picture(sender, instance, **kwargs):
+  if instance.picture:
+    if os.path.isfile(instance.picture.path):
+      os.remove(instance.picture.path)
--- store/views.py
+++ store/views.py
@@ -1,5 +1,8 @@
 from datetime import datetime
-from django.shortcuts import render
+from email import message
+from importlib.metadata import requires
+from urllib.request import Request
+from django.shortcuts import redirect, render, get_object_or_404
 from django.template import context
 from django.views.generic.base import(
   View, TemplateView, RedirectView
@@ -12,8 +15,10 @@ from django.views.generic.edit import (
 )
 from . import forms
 from datetime import datetime
-from .models import Books
+from .models import Books, Pictures
 from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib import messages
 
 # Create your views here.
 
@@ -82,14 +87,35 @@ class BookCreateView(CreateView):
       initial['name'] = 'sample'
       return initial
 
-class BookUpdateView(UpdateView):
+class BookUpdateView(SuccessMessageMixin, UpdateView):
 
   template_name = 'update_book.html'
   model = Books
   form_class = forms.BookUpdateForm
+  success_message = '更新に成功しました'
 
   def get_success_url(self):
     return reverse_lazy('store:edit_book', kwargs={'pk':self.object.id})
+
+  def get_success_message(self, cleaned_data):
+    print(cleaned_data)
+    return cleaned_data.get('name') + 'を更新しました'
+
+  def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      picture_form = forms.PictureUploadForm()
+      pictures = Pictures.objects.filter_by_book(book=self.object)
+      context['pictures'] = pictures
+      context['picture_form'] = picture_form
+      return context
+  
+  def post(self, request, *args, **kwargs):
+    #画像をアップロードする処理を書く
+    picture_form = forms.PictureUploadForm(request.POST or None, request.FILES or None)
+    if picture_form.is_valid() and request.FILES:
+      book = self.get_object() #更新中のBookがどのBookなのか取得できる
+      picture_form.save(book=book)
+    return super(BookUpdateView, self).post(request, *args, **kwargs)
 
 class BookDeleteView(DeleteView):
   model = Books
@@ -97,7 +123,6 @@ class BookDeleteView(DeleteView):
   success_url = reverse_lazy('store:list_books')
 
 class BookFormView(FormView):
-
   template_name = 'form_book.html'
   form_class = forms.BookForm
   success_url = reverse_lazy('store:list_books')
@@ -120,3 +145,12 @@ class BookRedirectView(RedirectView):
     if 'pk' in kwargs:
       return reverse_lazy('store:detail_book', kwargs={'pk': kwargs['pk']})
     return reverse_lazy('store:edit_book', kwargs={'pk':book.pk})
+
+def delete_picture(request, pk):
+  picture = get_object_or_404(Pictures, pk=pk)
+  picture.delete()
+  # import os
+  # if os.path.isfile(picture.picture.path):
+  #   os.remove(picture.picture.path)
+  messages.success(request, '画像を削除しました')
+  return redirect('store:edit_book', pk=picture.book.id)
